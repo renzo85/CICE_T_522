a97d55746ececcff291b44c00c9b126a08e49de5
unknown <rdcv54@gmail.com>
2021-07-08 21:27:02 +0200

    FEAT learn,receive damage

--- oop/pokemons.py
+++ oop/pokemons.py
@@ -15,6 +15,7 @@
 # Crear un pequeño menú que permita simular una batalla pokemon
 
 elements = ["fire", "grass", "water"]
+
 class Pokemon:
     def __init__(self, name, element, HP):
         self.name = name
@@ -28,7 +29,19 @@ class Pokemon:
     def learn(self, attack):
         self.attacks.append(attack)
 
+    def receive_damage(self, attack):
+        if attack.element == self.element:
+            self.HP -= attack.damage
+        else:
+            remain_elements = elements.copy() #se crea una copia de la lista para alterarla
+            remain_elements.remove(self.element) 
 
+            if attack.element == remain_elements[0]:
+                self.HP -= attack.damage * 1.5 
+            else:
+                self.HP -= attack.damage * 0.5
+
+       
 
 class Attack:
 
@@ -37,11 +50,12 @@ class Attack:
         self.element = element
         self.damage = damage
     
-    def __repr__(self): #repr se usa cuando no sea un str #su proposito es la manera de impresion del ataque
+    def __str__(self):
         return f"{self.name}"
 
-
-
+    def __repr__(self): #repr se usa cuando no sea un str #su proposito es la manera de impresion del ataque
+        return f"{self.name}"
+    
 
 charmander = Pokemon("Charmander", elements[0], 120)
 squirtle = Pokemon("Squirtle", elements[2], 140 )
@@ -52,8 +66,18 @@ razor_leaf = Attack("razor_Leaf", elements[1], 25)
 surf = Attack("surf", elements[2], 35)
 
 
-print (charmander) #print llama a str por detrás por eso no hay que ponerla necesariamente
-print (squirtle)
-print (bulbasaur)
-
 charmander.learn(flamethrower)
+charmander.learn(razor_leaf)
+charmander.learn(surf)
+
+
+print (charmander)
+# print (squirtle)
+# print (bulbasaur)
+
+for i, attack in enumerate(charmander.attacks):
+    print(f"{i + 1}. {attack}")
+
+user = int(input("Choose: "))
+bulbasaur.receive_damage(charmander.attacks[user -1])
+print(bulbasaur)
